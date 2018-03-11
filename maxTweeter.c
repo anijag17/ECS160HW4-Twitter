bc0ca0e205603c16b8a2a6ddfd6bd5f23a510485
anirudh <ajagdish@ucdavis.edu>
2018-03-11 13:50:03 -0700

    maxTweets not working

--- maxTweeter.c
+++ maxTweeter.c
@@ -37,18 +37,40 @@ char* getName(char* parse, int location){
 	for (int i = 0; i < location; i++){
 		token = strtok(NULL,",");
 	}
-	printf("n: %s\n",token);
+	//printf("n: %s\n",token);
 	return token;
 }
 
-// returns an index where the target exists, or -1
+
+// Either returns array index of targetName in array or -1 if not present
 int existsInArray(struct tweeter* arr, char* targetName, int size) 
 {
+    // works
     int index = -1;
- 
-
+    int i = 0;
+    while(i < size) 
+    {
+        if (strcmp(arr[i].name, targetName) == 0) 
+        {
+            index = i;
+            break;
+        }
+        i++;
+    }
     return index;
 }
+
+// Dynamically increases the size of the array with the space for 
+// one more struct object
+struct tweeter* increaseArray (struct tweeter* oldArr, int newSize)
+{
+    // works
+    struct tweeter* newArr = oldArr;
+    newArr = realloc(newArr, newSize * sizeof(struct tweeter));
+    return newArr;
+}
+
+
 
 int main(int argc, char** argv){
 	if (argc != 2){
@@ -66,15 +88,32 @@ int main(int argc, char** argv){
 
 	int location = getTweeterLocation(parse);
     
-    int arrSize = 1;
-    struct tweeter* array = malloc(sizeof(struct tweeter) * arrSize);
+    int size = 0;
+    size++;
+    struct tweeter* array = malloc(size * sizeof(struct tweeter)); 
 
 	printf("location of name is @ %d\n",location);
 	while (fgets(parse,sizeof(parse),fp)!= NULL){
 		char* currName = getName(parse,location);
+        printf("%s\n", currName);
+        /*
         // If currName exists in array, increment its count by 1, cont.
+        int index = existsInArray(array, currName, size);
+        if (index != -1)
+        {
+            array[index].tweetCount += 1;
+            continue;
+        }
         // Else, make a tweeter struct instance and add it to the array 
         // and initialize its count to 1; 
+        else if (index == -1) 
+        {
+            size++;
+            array = increaseArray(array, size);
+            array[size - 1].name = currName;
+            array[size - 1].tweetCount = 1;            
+        }
+        */
 	}
 	fclose(fp);
     return(0);
