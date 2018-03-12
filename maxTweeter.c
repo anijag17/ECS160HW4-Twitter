e1413bb72ddecb7286a9916f8d912681c2f56419
108Daniel <lidaniel916@gmail.com>
2018-03-11 18:20:01 -0700

    complete?

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,17 +1,66 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-struct tweeter {
-    char* name;
+static int SIZE = 20000;  //size will not exceed 20k
+
+struct tweeter {  //data
+    char name[56];
     int tweetCount;
 };
 
-/*
-input the first line of csv here
-return's location of the first "name" found
-return -1 if cannot find name
-*/
+struct pair {
+  char key[56];
+  struct tweeter data;
+};
+
+//https://stackoverflow.com/questions/2624192/good-hash-function-for-strings
+int hashf(char* name){
+  int size = strlen(name);
+  int hash = 7;
+  for (int i = 0; i < size; i++) {
+    hash = hash*31 + (int)name[i];
+  }
+  return abs(hash%SIZE);
+}
+
+//function
+int put(struct pair* table,char* name){
+  //printf("start put ");
+  //printf("%s %d\n",name,hashf(name));
+  int hash = hashf(name);
+  if (!strcmp(table[hash].key,name)){  //result of hash has same name
+    table[hash].data.tweetCount++;     //increase the count
+    return 0;
+  }
+  else if (!strcmp(table[hash].key,"\0")){ //bucket has not been initialized
+    strcpy(table[hash].key,name);
+    strcpy(table[hash].data.name,name);
+    table[hash].data.tweetCount++;
+    return 0;
+  }
+  else {  //collision :(
+    /* todo */
+    int inc = hash++;
+    while (strcmp(table[inc].key,name)){ //while they do not equal each other, check gthe next one
+      if (table[inc].data.tweetCount == 0){ //if it is empty,place in this bucket
+        strcpy(table[inc].key,name);
+        strcpy(table[inc].data.name,name);
+        table[inc].data.tweetCount++;
+        return 0;
+      }
+      else inc++;
+    }
+    //found here
+    strcpy(table[inc].key,name);
+    strcpy(table[inc].data.name,name);
+    table[inc].data.tweetCount++;
+    return 0;
+  }
+}
+
+
 int getTweeterLocation(char* parse){
 	int find = -1;
 	char* token;
@@ -28,9 +77,6 @@ int getTweeterLocation(char* parse){
 	return find;
 }
 
-/*
-given a line and the location counter, return the tweeter name value
-*/
 char* getName(char* parse, int location){
 	char* token;
 	token = strtok(parse,",");
@@ -41,39 +87,23 @@ char* getName(char* parse, int location){
 	return token;
 }
 
-
-// Either returns array index of targetName in array or -1 if not present
-int existsInArray(struct tweeter* arr, char* targetName, int size) 
-{
-    // works
-    int index = -1;
-    int i = 0;
-    while(i < size) 
-    {
-        if (strcmp(arr[i].name, targetName) == 0) 
-        {
-            index = i;
-            break;
-        }
-        i++;
+//bubble sort, but only the first 10
+int bubbleSort(struct pair* table){
+  for (int i = 0; i< 10; i++){
+    for (int j = i+1; j < SIZE; j++){
+      if (table[i].data.tweetCount < table[j].data.tweetCount){
+        struct pair temp = table[i];
+        table[i] = table[j];
+        table[j] = temp;
+      }
     }
-    return index;
+  }
+  return 0;
 }
-
-// Dynamically increases the size of the array with the space for 
-// one more struct object
-struct tweeter* increaseArray (struct tweeter* oldArr, int newSize)
-{
-    // works
-    struct tweeter* newArr = oldArr;
-    newArr = realloc(newArr, newSize * sizeof(struct tweeter));
-    return newArr;
-}
-
-
 
 int main(int argc, char** argv){
-	if (argc != 2){
+  //checks
+  if (argc != 2){
 		return -1;
 	}
 	FILE *fp=fopen(argv[1], "r");
@@ -85,37 +115,29 @@ int main(int argc, char** argv){
 	if( fgets (parse, sizeof(parse), fp)==NULL ){
 		return -1;
 	}
+  int location = getTweeterLocation(parse);
+  if (location == -1)
+    return -1;
+  //end of checks
+  struct pair table[SIZE];
+  struct pair init;
+  strcpy(init.key,"\0");
+  strcpy(init.data.name,"\0");
+  init.data.tweetCount = 0;
+  for (int i = 0; i < SIZE; i++){
+    table[i] = init;
+  }
+  //table initialized
+  char* curr;
+  while (fgets(parse,sizeof(parse),fp)!= NULL){
+    curr = getName(parse,location);
+    put(table,curr);
+  }
 
-	int location = getTweeterLocation(parse);
-    
-    int size = 0;
-    size++;
-    struct tweeter* array = malloc(size * sizeof(struct tweeter)); 
+  bubbleSort(table);
+  for (int i = 0; i < 10; i++){
+    printf("%s %d\n",table[i].key,table[i].data.tweetCount);
+  }
 
-	printf("location of name is @ %d\n",location);
-	while (fgets(parse,sizeof(parse),fp)!= NULL){
-		char* currName = getName(parse,location);
-        printf("%s\n", currName);
-        /*
-        // If currName exists in array, increment its count by 1, cont.
-        int index = existsInArray(array, currName, size);
-        if (index != -1)
-        {
-            array[index].tweetCount += 1;
-            continue;
-        }
-        // Else, make a tweeter struct instance and add it to the array 
-        // and initialize its count to 1; 
-        else if (index == -1) 
-        {
-            size++;
-            array = increaseArray(array, size);
-            array[size - 1].name = currName;
-            array[size - 1].tweetCount = 1;            
-        }
-        */
-	}
-	fclose(fp);
-    return(0);
 
 }
