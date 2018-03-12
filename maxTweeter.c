e4c87198453677beca11dc5cf5c2da79ef36af14
108Daniel <lidaniel916@gmail.com>
2018-03-11 19:35:46 -0700

    white space removal

--- maxTweeter.c
+++ maxTweeter.c
@@ -5,18 +5,18 @@
 
 static int SIZE = 20000;  //size will not exceed 20k
 
-struct tweeter {  //data
+struct tweeter {  //data of pair
     char name[56];
     int tweetCount;
 };
 
-struct pair {
+struct pair { //hash table entries
   char key[56];
   struct tweeter data;
 };
 
 //https://stackoverflow.com/questions/2624192/good-hash-function-for-strings
-int hashf(char* name){
+int hashf(char* name){  //used to hash string for table
   int size = strlen(name);
   int hash = 7;
   for (int i = 0; i < size; i++) {
@@ -25,10 +25,8 @@ int hashf(char* name){
   return abs(hash%SIZE);
 }
 
-//function
+//function to increment count if name is in hashtable
 int put(struct pair* table,char* name){
-  //printf("start put ");
-  //printf("%s %d\n",name,hashf(name));
   int hash = hashf(name);
   if (!strcmp(table[hash].key,name)){  //result of hash has same name
     table[hash].data.tweetCount++;     //increase the count
@@ -40,8 +38,7 @@ int put(struct pair* table,char* name){
     table[hash].data.tweetCount++;
     return 0;
   }
-  else {  //collision :(
-    /* todo */
+  else {  //collision, loop until found or empty bucket is found
     int inc = hash++;
     while (strcmp(table[inc].key,name)){ //while they do not equal each other, check gthe next one
       if (table[inc].data.tweetCount == 0){ //if it is empty,place in this bucket
@@ -60,7 +57,7 @@ int put(struct pair* table,char* name){
   }
 }
 
-
+//function finds the location of where "name" is in the header line
 int getTweeterLocation(char* parse){
 	int find = -1;
 	char* token;
@@ -77,6 +74,7 @@ int getTweeterLocation(char* parse){
 	return find;
 }
 
+//with the variable location, grab the string at a specific location in a line
 char* getName(char* parse, int location){
 	char* token;
 	token = strtok(parse,",");
@@ -87,7 +85,7 @@ char* getName(char* parse, int location){
 	return token;
 }
 
-//bubble sort, but only the first 10
+//bubble sort, but only the first 10 for O(n) solution for top ten tweets
 int bubbleSort(struct pair* table){
   for (int i = 0; i< 10; i++){
     for (int j = i+1; j < SIZE; j++){
@@ -103,24 +101,35 @@ int bubbleSort(struct pair* table){
 
 int main(int argc, char** argv){
   //checks
-  if (argc != 2){
+  if (argc != 2){ //makes sure there is only one csv argument
+    printf("Invalid Input Format\n");
 		return -1;
 	}
 	FILE *fp=fopen(argv[1], "r");
-	if (fp == NULL){
+	if (fp == NULL){ //cannot find specific csv
+    printf("Invalid Input Format\n");
 		return -1;
 	}
 
 	char parse[512];
-	if( fgets (parse, sizeof(parse), fp)==NULL ){
+	if( fgets (parse, sizeof(parse), fp)==NULL ){  //if empty file
+    printf("Invalid Input Format\n");
 		return -1;
 	}
+  while (!strcmp(parse,"\n")){  //skip empty lines before
+    if( fgets (parse, sizeof(parse), fp)==NULL ){  //if empty file
+      printf("Invalid Input Format\n");
+  		return -1;
+    }
+  }
   int location = getTweeterLocation(parse);
-  if (location == -1)
+  if (location == -1){  //could not find "name" in first row
+    printf("Invalid Input Format\n");
     return -1;
+  }
   //end of checks
   struct pair table[SIZE];
-  struct pair init;
+  struct pair init; //initialize all buckets to be at 0
   strcpy(init.key,"\0");
   strcpy(init.data.name,"\0");
   init.data.tweetCount = 0;
@@ -130,6 +139,10 @@ int main(int argc, char** argv){
   //table initialized
   char* curr;
   while (fgets(parse,sizeof(parse),fp)!= NULL){
+    if (!strcmp(parse,"\n"))  //skip empty lines
+    {
+      continue;
+    }
     curr = getName(parse,location);
     put(table,curr);
   }
@@ -139,5 +152,5 @@ int main(int argc, char** argv){
     printf("%s: %d\n",table[i].key,table[i].data.tweetCount);
   }
 
-
+  return 0;
 }
